4460715bfe82dda44601dcc7f2e974fd47fd6fac
Yann PETITJEAN <yann120@hotmail.fr>
2019-10-17 10:28:48 +0000

    allowed specific CORS from react app

--- server/index.js
+++ server/index.js
@@ -5,6 +5,12 @@ const port = 3001;
 const db = require('./queries/user');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "localhost:3000");
+  res.header("Access-Control-Allow-Origin", "ypetitjean.fr:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/', function (req, res) {
   res.send(JSON.stringify({info: 'Welcome to our matcha API'}));
