d519be38668e984502d9eacd65cadced8202bb0f
Yann PETITJEAN <yann120@hotmail.fr>
2019-10-27 16:37:22 +0000

    Tests on users are working. About to create the sexualOrientation files
    Co-authored-by: Ségolène Alquier <35140149+Segolene-Alquier@users.noreply.github.com>

--- server/rest/components(C-M-R)/user/utils.js
+++ server/rest/components(C-M-R)/user/utils.js
@@ -1,5 +1,5 @@
 const Gender = require("../gender/model")
-const gender = new Gender()
+const genderInstance = new Gender()
 
 class UserValidation {
     constructor(User) {
@@ -7,8 +7,6 @@ class UserValidation {
     }
 
     isEmail({input}) {
-        console.log(input)
-
         return new Promise(function(resolve, reject) {
             // eslint-disable-next-line no-control-regex
             const regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
@@ -16,37 +14,42 @@ class UserValidation {
             if (input.match(regex))
                 resolve({boolean: true})
             resolve({boolean: false, error: `The email you entered is not correct`})
-            })
+        })
 
     }
 
     isAlphaNum({input, input_name}) {
+        return new Promise(function(resolve, reject) {
         const regex = /^[0-9a-zA-Z]+$/;
-        if (input === '') return {boolean: false, error: `The ${input_name} is blank`}
+        if (input === '') resolve({boolean: false, error: `The ${input_name} is blank`})
         if (input.match(regex))
-            return true;
-        return ({boolean: false, error: `The ${input_name} must contain only letters and numbers`});
+            resolve({boolean: true});
+        resolve(({boolean: false, error: `The ${input_name} must contain only letters and numbers`}));
+        })
     }
 
     isAlpha({input, input_name}) {
+        return new Promise(function(resolve, reject) {
         const regex = /^[a-zA-Z]+$/;
-        if (input === '') return {boolean: false, error: `The ${input_name} is blank`}
+        if (input === '') resolve({boolean: false, error: `The ${input_name} is blank`})
         if (input.match(regex))
-            return {boolean: true};
-        return ({boolean: false, error: `The ${input_name} must contain only letters`});
+            resolve({boolean: true})
+        resolve(({boolean: false, error: `The ${input_name} must contain only letters`}))
+        })
     }
 
     isBoolean({input, input_name}) {
-        if (input === true || input === false)
-            return {boolean: true};
+        return new Promise(function(resolve, reject) {
+        if (input === 'true' || input === 'false')
+            resolve({boolean: true})
         else
-            return ({boolean: false, error: `The ${input_name} is not a boolean`}); 
+            resolve(({boolean: false, error: `The ${input_name} is not a boolean`}))
+        })
     }
 
     exists({input, input_name, model_instance}) {
         return new Promise(async function(resolve, reject) {
             let call = await model_instance.exists(input_name, input)
-            // console.log(call)
             if (call === true) 
                 resolve( {boolean: false, error: `The ${input_name} already exist`})
             else
@@ -54,33 +57,41 @@ class UserValidation {
         })
     }
 
-    async dontExists({input, input_name, model_instance}) {
-        let call = await model_instance.exists(input_name, input)
-        console.log(call)
-        if (call === true) 
-            return ({boolean: true})
-        else
-            return {boolean: false, error: `This ${model_instance} don't exist`};
+    doesntExist({input, input_name, model_instance}) {
+        return new Promise(async function(resolve, reject) {
+            input = parseInt(input)
+            let call = await model_instance.exists(input_name, input)
+            if (call === true) 
+                resolve({boolean: true})
+            else
+                resolve({boolean: false, error: `This ${model_instance} don't exist`});
+        })
     }
 
-    async fieldExists({input, input_name, user_instance}) {
-        let call = await user_instance.exists(input_name, input)
+    fieldExists({input, input_name, user_instance}) { // WIP
+        return new Promise(async function(resolve, reject) {
+          let call = await user_instance.exists(input_name, input)
+        })
     }
 
-    passwordFormat(password) {
-        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&.]).{8,10}$/;
-        if (!password) return false
-        if (password.match(regex))
-            return (true)
-        return (false)
+    passwordFormat({input}) {
+        return new Promise(function(resolve, reject) {
+            const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&.]).{8,10}$/;
+            if (!input) resolve({boolean: false, error: "This password is empty!"})
+            if (input.match(regex))
+                resolve({boolean: true})
+            resolve({boolean: false, error: "This password doesn't match!"})
+        })
     }
 
     rightLength({input, input_name, min_length, max_length}) {
-        if (input === '') return {boolean: false, error: `The ${input_name} is blank`}
-        if (input.length >= min_length && input.length <= max_length)
-            return ({boolean: true})
-        else
-        return ({boolean: false, error: `The ${input_name} must contain between ${min_length} and ${max_length} characters`})
+        return new Promise(function(resolve, reject) {
+            if (input === '') resolve({boolean: false, error: `The ${input_name} is blank`})
+            if (input.length >= min_length && input.length <= max_length)
+                resolve({boolean: true})
+            else
+            resolve({boolean: false, error: `The ${input_name} must contain between ${min_length} and ${max_length} characters`})
+        })
     }
 
     async createUserErrors(data) {
@@ -102,9 +113,26 @@ class UserValidation {
     async updateUserErrors(data) {
         const { firstname, surname, username, email, gender, sexualOrientation, description, interests, images, profilePicture, location, notificationMail, birthDate } = data
         var isEmail = this.isEmail({input: email})
-        var exists = this.exists({input: email, input_name: "email", model_instance: this.user})
+        var emailExists = this.exists({input: email, input_name: "email", model_instance: this.user})
+        var usernameExists = this.exists({input: username, input_name: "username", model_instance: this.user})
+        var usernameIsAlphaNum = this.isAlphaNum({input: username, input_name: 'user name'})
+        var usernameRightLength = this.rightLength({input: username, input_name: 'username', min_length: 2, max_length: 15, user_instance: this.user})
+        var firstnameIsAlpha = this.isAlpha({input: firstname, input_name: 'first name'})
+        var firstnameRightLength = this.rightLength({input: firstname, input_name: 'first name', min_length: 2, max_length: 40})
+        var surnameIsAlpha = this.isAlpha({input: surname, input_name: 'surname'})
+        var surnameRightLength = this.rightLength({input: surname, input_name: 'surname', min_length: 2, max_length: 40})
+        var descriptionRightLength = this.rightLength({input: description, input_name: 'surname', min_length: 2, max_length: 5000})
+        var genderDoesntExist = this.doesntExist({input: gender, input_name: 'id', model_instance: genderInstance})
+        var notificationIsBoolean = this.isBoolean({input: notificationMail, input_name: 'notification email'})
+        // Sexual orientation : pas de model encore
+        // Interests : pas de model encore
+        // Image: ne sait pas encore comment on va faire
+        // ProfilePicture: ne sait pas encore comment on va faire
+        // birthDate : ne sais pas encore le format de date        
         let errors = []
-        Promise.all([isEmail, exists]).then(values => console.log(values))
+
+        Promise.all([isEmail, emailExists, usernameExists, usernameIsAlphaNum, usernameRightLength, firstnameIsAlpha, firstnameRightLength, surnameIsAlpha, surnameRightLength, descriptionRightLength, genderDoesntExist, notificationIsBoolean]).then(values => console.log(values))
+
         // console.log(email);
         return ([])
     }
@@ -116,12 +144,12 @@ class UserValidation {
     //     this.inputTester({input: firstname, input_name: 'first name', min_length: 2, max_length: 40}, [this.isAlpha, this.rightLength], errors)
     //     this.inputTester({input: surname, input_name: 'surname', min_length: 2, max_length: 40}, [this.isAlpha, this.rightLength], errors)
         
-    //     // await ne fonctionne pas pour les fonctions de check if email exist et check if username exist
+    // //     // await ne fonctionne pas pour les fonctions de check if email exist et check if username exist
     //     await this.inputTester({input: username, input_name: 'username', min_length: 2, max_length: 15, user_instance: this.user}, [this.isAlphaNum, this.rightLength, this.userExists], errors)
-    //     this.inputTester({input: description, input_name: 'surname', min_length: 2, max_length: 5000}, [this.rightLength], errors)
-    //     // Gender a tester car await dans fonction en parametre ne fonctionne pas
+    // //     this.inputTester({input: description, input_name: 'surname', min_length: 2, max_length: 5000}, [this.rightLength], errors)
+    // //     // Gender a tester car await dans fonction en parametre ne fonctionne pas
     //     await this.inputTester({input: gender, input_name: 'gender'}, [this.dontExists], errors)        
-    //     // Sexual orientation : pas de model encore
+    // //     // Sexual orientation : pas de model encore
     //     // Interests : pas de model encore
     //     // Image: ne sait pas encore comment on va faire
     //     // ProfilePicture: ne sait pas encore comment on va faire
@@ -133,30 +161,14 @@ class UserValidation {
     //     return (errors)
     // }
     // WIP
-    // async inputTester(variable, functions, errors) {
-    //     if (variable['input'] === undefined)
-    //         return
-    //     const requests = functions.map((func) => {
-    //         return func(variable).then((a) => {
-    //             return a
-    //         })
-    //     })
-    //     return Promise.all(requests).then(a => console.log(JSON.stringify(a)))
-        // functions.forEach(async test_function => {
-        //     const result = await test_function(variable)
-        //     if (result['boolean'] === false)
-        //         errors.push(result['error'])
-        // });
-    // }
-    // async inputTester(variable, functions, errors) {
-    //     if (variable['input'] === undefined)
-    //         return
-    //     functions.forEach(async test_function => {
-    //         const result = await test_function(variable)
-    //         if (result['boolean'] === false)
-    //             errors.push(result['error'])
-    //     });
-    // }
+    async inputTester(variable, functions, errors) {
+        if (variable['input'] === undefined)
+            return
+        functions.forEach(oneFunction => {
+            oneFunction = oneFunction()
+        });
+    }
+
 
     filterInputValues(requester, values) {
         let authorized_values = []
