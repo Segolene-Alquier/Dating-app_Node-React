ab213720626f98de983c6b63632d727f19e2424e
Yann PETITJEAN <yann120@hotmail.fr>
2019-11-07 17:29:10 +0000

    code is cleaner

--- src/components/secretpage/secret-view.js
+++ src/components/secretpage/secret-view.js
@@ -3,29 +3,7 @@ import auth from '../auth';
 import axios from 'axios';
 
 const Secret = () => {
-  // const {getToken, loggedIn} = auth();
-
-  // const token = getToken()
-  // console.log("CURRENT TOKEN ", token)
-  // // console.log(loggedIn());
-  // // if (loggedIn()){
-  //   axios.get('http://localhost:3001/users', {
-  //     headers: {
-  //       "Content-type": "application/json; charset=UTF-8",
-  //       "x-access-token": token
-  //     }
-  //   }).then(({
-  //     data
-  //   }) => {
-  //     console.log(data)
-  //   })
   return <h1>Hello world!</h1>;
-  // }
-  // else {
-  //   return(
-  //     <h1>Tu n'est pas le bienvenue ici!</h1>
-  //   )
-  // }
 };
 
 export default Secret;
