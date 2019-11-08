de05438d0642c55a2edd76d8159def8c3ac1a963
Yann PETITJEAN <yann120@hotmail.fr>
2019-11-08 11:07:34 +0000

    backend send user data

--- server/rest/components(C-M-R)/auth/controller.js
+++ server/rest/components(C-M-R)/auth/controller.js
@@ -1,88 +1,91 @@
-const User = require("../user/model")
+const User = require('../user/model');
 const user = new User();
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const exjwt = require('express-jwt');
-const secret = 'mignon4ever'
+const secret = 'mignon4ever';
 
 async function booleanToken(request, response) {
-    let token = request.headers['x-access-token'] || request.headers['authorization']
-    console.log("CHECK TOKEN : ", token)
-    console.log("Headers : ", request.headers)
-    if (token) {
-        if (token.startsWith('Bearer ')) {
-            token = token.slice(7, token.length)
-        }
-        jwt.verify(token, secret, (err, decoded) => {
-            if (err) {
-                return (response.json({
-                    success: false,
-                    message: 'Token is not valid'
-                }))
-            }
-            else {
-                request.decoded = decoded
-                return (response.json({
-                    success: true,
-                    message: 'Token is valid'
-                }))
-            }
-        })
+  let token = request.headers['x-access-token'] || request.headers['authorization'];
+  if (token) {
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length);
     }
-    else {
-        return (response.json({
-            success: false,
-            message: 'Auth token not supplied' 
-        }))
-    }
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        return response.json({
+          success: false,
+          message: 'Token is not valid',
+        });
+      }
+      request.decoded = decoded;
+    });
+    const userId = request.decoded.userid;
+    const userData = await user.getByFiltered('id', userId, [
+      'id',
+      'firstname',
+      'surname',
+      'username',
+      'validated',
+      'suspended',
+    ]);
+
+    return response.json({
+      success: true,
+      message: 'Token is valid',
+      data: userData[0],
+    });
+  } else {
+    return response.json({
+      success: false,
+      message: 'Auth token not supplied',
+    });
+  }
 }
 
 async function login(request, response) {
-    const { username, password } = request.body
+  const { username, password } = request.body;
 
-    console.log("User submitted: ", username, password);
-    try {
-        let visitor = await user.getBy('username', username)
-        if (visitor.length <= 0) {
-            console.log(username, " doesn't exist")
-            response.json(false)
-            return
-        }
-        visitor = visitor[0]
-        if (visitor['password'] === password) {
-            let token = jwt.sign({userid: visitor['id']}, 'mignon4ever', {expiresIn: "24h"})
-            console.log(token)
-            response.json({
-                success: true,
-                token: token,
-                err: null
-            })
-        }
-        else {
-            console.log("Entered Password and Hash do not match!");
-            response.status(401).json({
-                success: false,
-                token: null,
-                err: 'Entered Password and Hash do not match!'
-            })
-        }
-    } 
-    catch (err) {
-        console.log(err);
-        response.status(206).send(err);
+  console.log('User submitted: ', username, password);
+  try {
+    let visitor = await user.getBy('username', username);
+    if (visitor.length <= 0) {
+      console.log(username, " doesn't exist");
+      response.json(false);
+      return;
     }
+    visitor = visitor[0];
+    if (visitor['password'] === password) {
+      let token = jwt.sign({ userid: visitor['id'] }, 'mignon4ever', { expiresIn: '24h' });
+      console.log(token);
+      response.json({
+        success: true,
+        token: token,
+        err: null,
+      });
+    } else {
+      console.log('Entered Password and Hash do not match!');
+      response.status(401).json({
+        success: false,
+        token: null,
+        err: 'Entered Password and Hash do not match!',
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    response.status(206).send(err);
+  }
 }
 
 async function logout(request, response) {
-    try {
-        // let call = await gender.getAll()
-        // response.status(200).json(call)
-    } 
-    catch (err) {
-        console.log(err);
-        response.status(206).send(err);
-    }
+  try {
+    // let call = await gender.getAll()
+    // response.status(200).json(call)
+  } catch (err) {
+    console.log(err);
+    response.status(206).send(err);
+  }
 }
 
-module.exports.login = login
-module.exports.logout = logout
-module.exports.booleanToken = booleanToken
+module.exports.login = login;
+module.exports.logout = logout;
+module.exports.booleanToken = booleanToken;
--- server/rest/components(C-M-R)/user/model.js
+++ server/rest/components(C-M-R)/user/model.js
@@ -1,95 +1,114 @@
-const {db, pgp} = require("../../../config/database");
+const { db, pgp } = require('../../../config/database');
 
 class User {
-
   isValidType(type) {
-    const authorized_types = ['id', 'email', 'username']
-    return authorized_types.some((authorized_type) => {
-      return type === authorized_type
-    })
+    const authorized_types = ['id', 'email', 'username'];
+    return authorized_types.some(authorized_type => {
+      return type === authorized_type;
+    });
   }
 
-  async  getBy(type, value) {
+  async getBy(type, value) {
     try {
       let result;
       if (!this.isValidType(type)) {
         console.log(`User.getBy(): ${type} is not an authorized type`);
-        return (null);
+        return null;
       }
-      console.log(`SELECT * FROM public."User" WHERE ${type} = ${value}`)
-      result = await db.any(`SELECT * FROM public."User" WHERE $1:name = $2`, [type, value])
-      return (result)
-    }
-    catch (err) {
-      console.log(err, "in model User.getBy()");
+      console.log(`SELECT * FROM public."User" WHERE ${type} = ${value}`);
+      result = await db.any(`SELECT * FROM public."User" WHERE $1:name = $2`, [type, value]);
+      return result;
+    } catch (err) {
+      console.log(err, 'in model User.getBy()');
     }
   }
-
-  async  updateById(id, values) {
+  async getByFiltered(type, value, inputs) {
     try {
-      console.log(values)
-      const query = pgp.helpers.update(values,values,`User`) + ' WHERE id = $1';
-      console.log(query)
-      console.log(db.any(query,[id]))
-    }
-    catch (err) {
-      console.log(err, "in model User.updateById()");
+      let result;
+      if (!this.isValidType(type)) {
+        console.log(`User.getBy(): ${type} is not an authorized type`);
+        return null;
+      }
+      console.log(`SELECT ${inputs} FROM public."User" WHERE ${type} = ${value}`);
+      result = await db.any(`SELECT $1:name FROM public."User" WHERE $2:name = $3`, [
+        inputs,
+        type,
+        value,
+      ]);
+      return result;
+    } catch (err) {
+      console.log(err, 'in model User.getBy()');
     }
   }
 
-  async  exists(type, value) {
+  async updateById(id, values) {
+    try {
+      console.log(values);
+      const query = pgp.helpers.update(values, values, `User`) + ' WHERE id = $1';
+      console.log(query);
+      console.log(db.any(query, [id]));
+    } catch (err) {
+      console.log(err, 'in model User.updateById()');
+    }
+  }
+
+  async exists(type, value) {
     try {
       let result;
       if (!value) return false;
       // console.log(type)
       if (!this.isValidType(type)) {
         console.log(`User.exists(): ${type} is not an authorized type`);
-        return (null);
+        return null;
       }
-      console.log(`SELECT exists(SELECT from public."User" WHERE ${type} = ${value})`)
-      result = await db.any(`SELECT exists(SELECT from public."User" WHERE $1:name = $2);`, [type, value])
-      return (result[0].exists)
-    }
-    catch (err) {
-      console.log(err, "in model User.exists()");
+      console.log(`SELECT exists(SELECT from public."User" WHERE ${type} = ${value})`);
+      result = await db.any(`SELECT exists(SELECT from public."User" WHERE $1:name = $2);`, [
+        type,
+        value,
+      ]);
+      return result[0].exists;
+    } catch (err) {
+      console.log(err, 'in model User.exists()');
     }
   }
 
-  async  getAll() {
+  async getAll() {
     try {
       let result;
-      console.log('SELECT * FROM public."User"')
-      result = await db.any('SELECT * FROM public."User"')
-      return (result)
-    }
-    catch (err) {
-      console.log(err, "in model User.getAll()");
+      console.log('SELECT * FROM public."User"');
+      result = await db.any('SELECT * FROM public."User"');
+      return result;
+    } catch (err) {
+      console.log(err, 'in model User.getAll()');
     }
   }
 
-  async create({firstname, surname, username, password, email}) {
+  async create({ firstname, surname, username, password, email }) {
     try {
-      console.log(`INSERT INTO public."User" (firstname, surname, username, password, email) VALUES (${firstname}, ${surname}, ${username}, ${password}, ${email})`)
-      await db.any('INSERT INTO public."User" (firstname, surname, username, password, email) VALUES ($1, $2, $3, $4, $5)', [firstname, surname, username, password, email])      
-      return ({created: true})
-    }
-    catch (err) {
-      console.log(err, "in model User.create()");
-      return ({created: false, error: err})
+      console.log(
+        `INSERT INTO public."User" (firstname, surname, username, password, email) VALUES (${firstname}, ${surname}, ${username}, ${password}, ${email})`,
+      );
+      await db.any(
+        'INSERT INTO public."User" (firstname, surname, username, password, email) VALUES ($1, $2, $3, $4, $5)',
+        [firstname, surname, username, password, email],
+      );
+      return { created: true };
+    } catch (err) {
+      console.log(err, 'in model User.create()');
+      return { created: false, error: err };
     }
   }
 
   async delete(id) {
     try {
-      console.log(`DELETE FROM public."User" WHERE id = ${id}`)
-      await db.any('DELETE FROM public."User" WHERE id = $1 ', [id])      
-      return ({deleted: true})
-    }
-    catch (err) {
-      console.log(err, "in model User.delete()");
-      return ({deleted: false, error: err})
+      console.log(`DELETE FROM public."User" WHERE id = ${id}`);
+      await db.any('DELETE FROM public."User" WHERE id = $1 ', [id]);
+      return { deleted: true };
+    } catch (err) {
+      console.log(err, 'in model User.delete()');
+      return { deleted: false, error: err };
     }
   }
 }
 
-module.exports = User
+module.exports = User;
