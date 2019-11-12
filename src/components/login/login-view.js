3678a2ac0adf4d830fa40fe3cd44c99ffaf1ee9c
Yann PETITJEAN <yann120@hotmail.fr>
2019-11-12 15:57:09 +0000

    fix linter errors

--- src/components/login/login-view.js
+++ src/components/login/login-view.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
 import useLoginForm from './login-container';
 
 const Login = () => {
