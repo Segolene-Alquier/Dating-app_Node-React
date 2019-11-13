70664ae892d46b24e6f12e7faf699ef52177f02f
Segolene Alquier <segolene.alquier@yahoo.com>
2019-11-13 17:59:43 +0000

    added material ui to sign up view. need to reduce the length with a grid

--- src/components/signup/signup-view.js
+++ src/components/signup/signup-view.js
@@ -1,6 +1,40 @@
-import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+
 import useSignUpForm from './signup-container';
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Signup = () => {
   const signup = success => {
@@ -9,84 +43,114 @@ const Signup = () => {
     }
   };
   const { inputs, handleInputChange, handleSubmit } = useSignUpForm(signup);
+  const classes = useStyles();
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="firstname">
-          First Name
-          <input
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <TextField
+            fullWidth
+            margin="normal"
+            htmlFor="firstname"
             type="text"
             name="firstname"
             onChange={handleInputChange}
             value={inputs.firstname}
             id="firstname"
+            variant="outlined"
+            label="First Name"
             required
           />
-        </label>
-        <label htmlFor="surname">
-          Last Name
-          <input
+          <TextField
+            fullWidth
+            margin="normal"
+            htmlFor="surname"
             type="text"
-            name="surname"
-            id="surname"
+            name="firstname"
             onChange={handleInputChange}
             value={inputs.surname}
+            id="surname"
+            label="Last Name"
+            variant="outlined"
             required
           />
-        </label>
-        <label htmlFor="username">
-          Username
-          <input
+          <TextField
+            fullWidth
+            margin="normal"
+            htmlFor="username"
             type="text"
             name="username"
-            id="username"
             onChange={handleInputChange}
             value={inputs.username}
+            id="username"
+            variant="outlined"
             required
           />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="email">
-          Email Address
-          <input
+          <TextField
+            fullWidth
+            margin="normal"
+            htmlFor="email"
             type="email"
             name="email"
-            id="email"
             onChange={handleInputChange}
             value={inputs.email}
+            id="email"
+            variant="outlined"
+            label="Email Address"
             required
           />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password1">
-          Password
-          <input
+          <TextField
+            fullWidth
+            margin="normal"
+            htmlFor="password1"
             type="password"
             name="password1"
-            id="password1"
+            label="Password"
             onChange={handleInputChange}
             value={inputs.password1}
+            id="password1"
+            variant="outlined"
             required
           />
-        </label>
-      </div>
-      <div>
-        <label htmlFor="password2">
-          Re-enter Password
-          <input
+          <TextField
+            fullWidth
+            margin="normal"
+            htmlFor="password2"
             type="password"
             name="password2"
-            id="password2"
             onChange={handleInputChange}
             value={inputs.password2}
+            id="password2"
+            variant="outlined"
+            label="Re-enter password"
             required
           />
-        </label>
+
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
       </div>
-      <button type="submit">Sign Up</button>
-    </form>
+    </Container>
   );
 };
 
