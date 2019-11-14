96761c9c390d08b209e417eff097caea8ada7e81
Segolene Alquier <segolene.alquier@yahoo.com>
2019-11-14 16:36:30 +0000

    added tabs to the profile page that contain the two colums

--- src/components/profile/profile-view.js
+++ src/components/profile/profile-view.js
@@ -6,7 +6,9 @@ import Avatar from '@material-ui/core/Avatar';
 // import FaceIcon from '@material-ui/icons/Face';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
-// import Mamie from './../../assets/images/mamie.jpeg';
+import { Tabs, Tab } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+// import PropTypes from 'prop-types';
 
 // import { sizing } from '@material-ui/system';
 
@@ -28,14 +30,53 @@ const useStyles = makeStyles(theme => ({
     fontSize: '1em',
     padding: theme.spacing(1),
   },
-
   boxUpProfile: {
+    margin: theme.spacing(1),
+  },
+  tabs: {
+    margin: theme.spacing(1),
+  },
+  divider: {
     margin: theme.spacing(1),
   },
 }));
 
+function TabPanel(props) {
+  const { children, value, index } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      //   {...other}
+    >
+      <Box p={3}>{children}</Box>
+    </Typography>
+  );
+}
+
+// TabPanel.propTypes = {
+//   children: PropTypes.node,
+//   index: PropTypes.any.isRequired,
+//   value: PropTypes.any.isRequired,
+// };
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const Profile = () => {
   const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <>
@@ -79,7 +120,37 @@ const Profile = () => {
           </Grid>
         </Grid>
       </Box>
-      <Divider variant="middle" />
+      <Divider className={classes.divider} />
+      <Tabs
+        width="100%"
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+        className={classes.tabs}
+      >
+        <Tab label="About me" {...a11yProps(0)} />
+        <Tab label="Parameters" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <Grid container>
+          <Grid container sm={8} bgcolor="primary.main" direction="column">
+            Coucou tout a propos de moi
+          </Grid>
+          <Grid container sm={4}>
+            WIP
+          </Grid>
+        </Grid>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Grid container>
+          <Grid container sm={8} bgcolor="primary.main" direction="column">
+            Mes petites infos
+          </Grid>
+          <Grid container sm={4}>
+            WIP
+          </Grid>
+        </Grid>
+      </TabPanel>
     </>
   );
 };
