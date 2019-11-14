de229ab8ceb20f4b2610911486ded53066bf4d1d
Yann PETITJEAN <yann120@hotmail.fr>
2019-11-14 13:52:29 +0000

    verifyConfirmationToken in model userValidation is working

--- server/rest/components(C-M-R)/userValidation/model.js
+++ server/rest/components(C-M-R)/userValidation/model.js
@@ -1,5 +1,8 @@
 const TokenGenerator = require('uuid-token-generator');
 const { db } = require('../../../config/database');
+const User = require('../user/model');
+
+const user = new User();
 
 const tokgen = new TokenGenerator(256, TokenGenerator.BASE62);
 
@@ -23,10 +26,55 @@ class UserValidation {
       );
       return { created: true, token };
     } catch (err) {
-      console.log(err, 'in model User.create()');
+      console.log(err, 'in model UserValidation.create()');
       return { created: false, error: err };
     }
   }
-}
 
+  async verifyConfirmationToken({ token }) {
+    if (token === undefined) {
+      console.log('The token is not defined');
+      return {
+        success: false,
+        error: 'The token is not defined',
+      };
+    }
+    try {
+      return db
+        .one(
+          'DELETE FROM  public."UserValidation" WHERE "validationKey" = $1 RETURNING "userId"',
+          token,
+        )
+        .then(({ userId }) => {
+          user.updateById(userId, { validated: true });
+          return {
+            success: true,
+            error: 'The account is now validated!',
+          };
+        })
+        .catch(error => {
+          if (error.received === 0) {
+            return {
+              success: false,
+              error: 'The confirmation link is not valid',
+            };
+          }
+          console.log(error, 'in model UserValidation.create()');
+          return {
+            success: false,
+            error,
+          };
+        });
+    } catch (err) {
+      console.log(err, 'in model UserValidation.create()');
+      return { success: false, error: err };
+    }
+  }
+}
+// const uv = new UserValidation();
+// console.log(
+//   uv.verifyConfirmationToken({
+//     token: 'DKiVPwcK9MJ14o9xNaEp5rhvIuErfbrLwzvjP0RjF1W',
+//   }),
+// );
 module.exports = UserValidation;
