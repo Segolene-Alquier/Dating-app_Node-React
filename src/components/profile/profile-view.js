19b6ee9ba61c5b35ff5f06b7256c69b1433242a9
Segolene Alquier <segolene.alquier@yahoo.com>
2019-11-14 19:26:42 +0000

    finished the about me tab on profile page, fully responsive. Need to do the parameters tab

--- src/components/profile/profile-view.js
+++ src/components/profile/profile-view.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Box from '@material-ui/core/Box';
-// import Button from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
-// import FaceIcon from '@material-ui/icons/Face';
 import Divider from '@material-ui/core/Divider';
 import { makeStyles } from '@material-ui/core/styles';
 import { Tabs, Tab } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
-// import PropTypes from 'prop-types';
-
-// import { sizing } from '@material-ui/system';
+import Radio from '@material-ui/core/Radio';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormLabel from '@material-ui/core/FormLabel';
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -33,11 +35,35 @@ const useStyles = makeStyles(theme => ({
   boxUpProfile: {
     margin: theme.spacing(1),
   },
+  picture: {
+    padding: theme.spacing(1),
+  },
+  profilePicture: {
+    border: '3px solid',
+    borderColor: theme.palette.secondary.main,
+    boxSizing: 'border-box',
+  },
   tabs: {
     margin: theme.spacing(1),
   },
   divider: {
     margin: theme.spacing(1),
+  },
+  modifyPictureButton: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: 'white',
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    fontSize: '1em',
+  },
+  summaryField: {
+    width: '90%',
+  },
+  formControl: {
+    marginBottom: theme.spacing(3),
   },
 }));
 
@@ -76,6 +102,14 @@ const Profile = () => {
   const [value, setValue] = React.useState(0);
   const handleChange = (event, newValue) => {
     setValue(newValue);
+  };
+  const [gender, setGender] = React.useState('female');
+  const handleChangeGender = (event, newGender) => {
+    setGender(newGender);
+  };
+  const [sexualPreference, setSexualPreference] = React.useState('both');
+  const handleSexualPreference = (event, newSexualPreference) => {
+    setSexualPreference(newSexualPreference);
   };
 
   return (
@@ -134,10 +168,130 @@ const Profile = () => {
       <TabPanel value={value} index={0}>
         <Grid container>
           <Grid container sm={8} bgcolor="primary.main" direction="column">
-            Coucou tout a propos de moi
+            <Typography variant="subtitle1">
+              <Box fontWeight="fontWeightBold">Gender</Box>
+            </Typography>
+            <FormControl component="fieldset" className={classes.formControl}>
+              <RadioGroup
+                aria-label="gender"
+                name="gender"
+                value={gender}
+                onChange={handleChangeGender}
+                row
+              >
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  value="non-binary"
+                  control={<Radio />}
+                  label="Non-binary"
+                />
+              </RadioGroup>
+            </FormControl>
+            <Typography variant="subtitle1">
+              <Box fontWeight="fontWeightBold">I am looking for</Box>
+            </Typography>
+            <FormControl component="fieldset" className={classes.formControl}>
+              <RadioGroup
+                aria-label="gender"
+                name="gender"
+                value={sexualPreference}
+                onChange={handleSexualPreference}
+                row
+              >
+                <FormControlLabel
+                  value="femalePreference"
+                  control={<Radio />}
+                  label="Female"
+                />
+                <FormControlLabel
+                  value="malePreference"
+                  control={<Radio />}
+                  label="Male"
+                />
+                <FormControlLabel
+                  value="both"
+                  control={<Radio />}
+                  label="Both"
+                />
+              </RadioGroup>
+            </FormControl>
+            <Typography variant="subtitle1">
+              <Box fontWeight="fontWeightBold">My self-summary</Box>
+            </Typography>
+            <TextField
+              id="outlined-multiline-static"
+              multiline
+              rows="4"
+              className={classes.summaryField}
+              margin="normal"
+              variant="outlined"
+            />
+            <Box>
+              <Button variant="contained" color="secondary" size="medium">
+                Save changes
+              </Button>
+            </Box>
           </Grid>
           <Grid container sm={4}>
-            WIP
+            <Typography variant="subtitle1">
+              <Box fontWeight="fontWeightBold">My pictures</Box>
+            </Typography>
+            <Grid container>
+              <Grid container xs={6} sm={6} className={classes.picture}>
+                <img
+                  src="https://image.noelshack.com/fichiers/2019/46/4/1573736912-mamie.jpeg"
+                  alt="My profile"
+                  width="100%"
+                />
+              </Grid>
+              <Grid container xs={6} sm={6} className={classes.picture}>
+                <img
+                  className={classes.profilePicture}
+                  src="https://image.noelshack.com/fichiers/2019/46/4/1573736912-mamie.jpeg"
+                  alt="My profile"
+                  width="100%"
+                />
+              </Grid>
+              <Grid container xs={6} sm={6} className={classes.picture}>
+                <img
+                  src="https://image.noelshack.com/fichiers/2019/46/4/1573736912-mamie.jpeg"
+                  alt="My profile"
+                  width="100%"
+                />
+              </Grid>
+              <Grid container xs={6} sm={6} className={classes.picture}>
+                <img
+                  src="https://image.noelshack.com/fichiers/2019/46/4/1573736912-mamie.jpeg"
+                  alt="My profile"
+                  width="100%"
+                />
+              </Grid>
+              <Grid container xs={6} sm={6} className={classes.picture}>
+                <img
+                  src="https://image.noelshack.com/fichiers/2019/46/4/1573736912-mamie.jpeg"
+                  alt="My profile"
+                  width="100%"
+                />
+              </Grid>
+              <Grid container xs={6} sm={6} className={classes.picture}>
+                <Box
+                  bgcolor="secondary.main"
+                  width="100%"
+                  className={classes.modifyPictureButton}
+                >
+                  <p>Modifier les photos</p>
+                </Box>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </TabPanel>
