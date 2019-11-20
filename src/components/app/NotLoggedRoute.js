33162e995c0bc413492221fb6a6006d4439b110d
Yann PETITJEAN <yann120@hotmail.fr>
2019-11-20 15:08:24 +0000

    fix redirecting on notloggedroute if token is expired

--- src/components/app/NotLoggedRoute.js
+++ src/components/app/NotLoggedRoute.js
@@ -2,10 +2,12 @@
 import React, { useState, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { AuthContext } from './AuthContext';
+import { isTokenExpired } from '../auth/AuthContainer';
 
 const NotLoggedRoute = ({ component: Component, ...rest }) => {
   const authContext = useContext(AuthContext);
-  const defaultValue = authContext.token !== null;
+  const defaultValue =
+    authContext.token !== null && !isTokenExpired(authContext.token);
   const [secureAuth, setSecureAuth] = useState(defaultValue);
   if (secureAuth === true) {
     authContext.userData.then(data => setSecureAuth(data.success));
