32677e04ebcc9b08cca0c7c4abab47e10c80be16
Segolene Alquier <segolene.alquier@yahoo.com>
2019-11-25 19:12:54 +0000

    finished update of left column fields in profile page

--- src/components/profile/profile-container.js
+++ src/components/profile/profile-container.js
@@ -1,6 +1,8 @@
 import { useState, use } from 'react';
 import _ from 'lodash';
 import axios from 'axios';
+import { toast } from 'react-toastify';
+
 // import { AuthContext } from '../app/AuthContext';
 
 const UseProfileForm = (userData, token) => {
@@ -16,8 +18,6 @@ const UseProfileForm = (userData, token) => {
 
   const handleProfileChange = event => {
     event.persist();
-    console.log('name: ', event.target.name);
-    console.log('value: ', event.target.value);
     const newInput = {
       ...profile,
       [event.target.name]: event.target.value,
@@ -32,10 +32,10 @@ const UseProfileForm = (userData, token) => {
 
   const handleSubmitParameters = event => {
     event.persist();
-    // console.log('submit name: ', profile);
     console.log('changedFields: ', changedFields);
-
-    if (event) {
+    if (_.isEmpty(changedFields)) {
+      toast.error("You didn't make any changes!");
+    } else if (event) {
       event.preventDefault();
       axios
         .put('http://localhost:3001/users', changedFields, {
@@ -44,8 +44,16 @@ const UseProfileForm = (userData, token) => {
             'x-access-token': token,
           },
         })
-        .then(({ data }) => {
-          console.log('data bitch', data);
+        .then(response => {
+          if (response.data.success === true) {
+            toast.success(response.data.message);
+            setChangedFields({});
+          } else {
+            console.log(response);
+            response.data.errors.forEach(error => {
+              toast.error(error);
+            });
+          }
         });
     }
   };
