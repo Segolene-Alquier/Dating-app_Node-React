0e605b86f36abdb502faef4310bbe41c6b38b984
Segolene Alquier <segolene.alquier@yahoo.com>
2020-02-04 17:36:59 +0000

    created function getMessagesFromMatchId : need to created messages in db and make the feature

--- server/rest/components(C-M-R)/chatroom/controller.js
+++ server/rest/components(C-M-R)/chatroom/controller.js
@@ -13,6 +13,17 @@ async function getMatchsFromCurrentUser(request, response) {
   }
 }
 
+async function getMessagesFromMatchId(request, response) {
+  try {
+    const id = request.decoded.userid;
+    const call = await matchs.getBy(['user1', 'user2'], id);
+    response.status(200).json(call);
+  } catch (err) {
+    console.log(err);
+    response.status(206).send(err);
+  }
+}
+
 // async function getMatchById(request, response) {
 //   const id = parseInt(request.params.id, 10);
 //   try {
