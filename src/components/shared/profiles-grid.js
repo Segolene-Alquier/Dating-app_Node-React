bcf3e3db001924ac3537ae5979ddc867db51ec9c
Yann PETITJEAN <yann120@hotmail.fr>
2020-02-25 11:56:58 +0000

    add infinite scroll to profiles grid

--- src/components/shared/profiles-grid.js
+++ src/components/shared/profiles-grid.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import _ from 'lodash';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import MediaCard from './components/media-card';
 
 const ProfilesGrid = ({
@@ -11,29 +13,48 @@ const ProfilesGrid = ({
   handleLike,
   type,
 }) => {
+  const [displayedProfiles, setDisplayedProfiles] = useState([]);
+  const displayMoreProfiles = () => {
+    setDisplayedProfiles(profiles.slice(0, displayedProfiles.length + 20));
+  };
+  useEffect(() => {
+    displayMoreProfiles();
+    // eslint-disable-next-line
+  }, [profiles]);
   return (
     <div className={classes.wrapper}>
       <Container>
-        <Grid container spacing={3}>
-          {_.map(profiles, profile => (
-            <Grid
-              item
-              xs={12}
-              sm={6}
-              md={3}
-              lg={3}
-              className={classes.center}
-              key={profile.username}
-            >
-              <MediaCard
-                field={profile}
-                profile={currentUserProfile}
-                handleLike={handleLike}
-                type={type}
-              />
-            </Grid>
-          ))}
-        </Grid>
+        <InfiniteScroll
+          dataLength={displayedProfiles.length}
+          next={displayMoreProfiles}
+          hasMore
+          loader={
+            <div className={classes.progress}>
+              <CircularProgress color="secondary" />
+            </div>
+          }
+        >
+          <Grid container spacing={3}>
+            {_.map(displayedProfiles, profile => (
+              <Grid
+                item
+                xs={12}
+                sm={6}
+                md={3}
+                lg={3}
+                className={classes.center}
+                key={profile.username}
+              >
+                <MediaCard
+                  field={profile}
+                  profile={currentUserProfile}
+                  handleLike={handleLike}
+                  type={type}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </InfiniteScroll>
       </Container>
     </div>
   );
