92eb5c7aa0ddc9d4a13857d2a589d8252fe0af91
Yann PETITJEAN <yann120@hotmail.fr>
2020-02-26 14:30:48 +0000

    add production mode to database

--- server/config/database.js
+++ server/config/database.js
@@ -2,16 +2,20 @@ const pgp = require('pg-promise')({
   // Initialization Options
 });
 
-const cn = {
-  host: process.env.DB_HOST,
-  port: 5432,
-  database: 'matcha',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  currentSchema: 'public',
-};
-
-const db = pgp(cn);
+let db;
+if (process.env.ENVIRONMENT === 'production' && process.env.DATABASE_URL) {
+  db = pgp(process.env.DATABASE_URL);
+} else {
+  const cn = {
+    host: process.env.DB_HOST,
+    port: 5432,
+    database: 'matcha',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    currentSchema: 'public',
+  };
+  db = pgp(cn);
+}
 
 module.exports = { db, pgp };
 
