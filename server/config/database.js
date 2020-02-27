66ec3cb96ec8ddfc07551f552f49e51eb817e1ac
Yann PETITJEAN <yann120@hotmail.fr>
2020-02-27 10:33:53 +0000

    change database for production mode

--- server/config/database.js
+++ server/config/database.js
@@ -2,11 +2,11 @@ const pgp = require('pg-promise')({
   // Initialization Options
 });
 
-let db;
+let cn;
 if (process.env.ENVIRONMENT === 'production' && process.env.DATABASE_URL) {
-  db = pgp(process.env.DATABASE_URL);
+  cn = process.env.DATABASE_URL;
 } else {
-  const cn = {
+  cn = {
     host: process.env.DB_HOST,
     port: 5432,
     database: 'matcha',
@@ -14,19 +14,7 @@ if (process.env.ENVIRONMENT === 'production' && process.env.DATABASE_URL) {
     password: process.env.DB_PASSWORD,
     currentSchema: 'public',
   };
-  db = pgp(cn);
 }
+const db = pgp(cn);
 
 module.exports = { db, pgp };
-
-// const Pool = require('pg').Pool
-// const pool = new Pool({
-//     user: 'yann',
-//     host: 'localhost',
-//     database: 'matcha',
-//     password: 'Bbu4juillet',
-//     currentSchema: 'public',
-//     port: 5432
-// })
-
-// module.exports = pool
